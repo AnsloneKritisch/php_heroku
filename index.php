0729c9ea25b816b0477fa37a23567a6aed2daa1d
AnsloneKritisch <digveshoptimus@gmail.com>
2022-10-12 19:56:15 +0530

    new

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-
-<?php
-
-echo "Hello World" ;
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time To Check It</title>
+</head>
+<body>
+    <h1>Work on Progress</h1>
+    <h2>Just Wait Baby</h2>
+</body>
+</html>
